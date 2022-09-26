a24c26e474234cc5ca7818129aff91b624a626cc
William <william.mallevays@epitech.eu>
2022-09-26 11:39:43 +0200

    {~} Test github action.

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 const port = 4000
 
 app.get('/', (req, res) => {
-  res.send('hello aaaaald')
+  res.send('hello world')
 })
 
 app.listen(port, () => {
