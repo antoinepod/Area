e157bbd3a06abd75ece600365393bed7b188d4fe
Antoine <antoine.podvin@epitech.eu>
2022-10-27 19:04:10 +0200

    {+ mobile} Adding home screen with app logo displayed.

---
+++ mobile/src/components/home/Home.js
@@ -0,0 +1,83 @@
+import React, {Node, useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  StatusBar,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  ScrollView,
+  Animated,
+  useWindowDimensions,
+} from 'react-native';
+import CreateCard from './Cards';
+
+export default function ({ navigation }) {
+
+  const {width} = useWindowDimensions();
+
+  const images = React.useMemo(() => ([
+    require('../../../assets/YouGram.png'),
+    require('../../../assets/YouGram.png'),
+    require('../../../assets/YouGram.png'),
+    require('../../../assets/YouGram.png'),
+    require('../../../assets/YouGram.png'),
+    require('../../../assets/YouGram.png')
+  ]), []);
+
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#020410" />
+      <Image style={styles.image} source={require("../../../assets/area_logo.png")} />
+      <Text style={styles.title}> Your cards </Text>
+      <Animated.FlatList
+        onScroll={Animated.event([{nativeEvent:  {contentOffset: {x: scrollX}}}], {useNativeDriver: true})}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={true}
+        renderItem={({item}) => {
+          return (
+              <View style={{
+                // alignItems: "center",
+                justifyContent: "center",
+                width,
+                marginTop: "5%",
+                marginBottom: "15%"
+              }}>
+                <CreateCard 
+                  title="YouGram"
+                  image={item}
+                  description="YouGram is a service that allows you to receive Telegram message when a youtuber post a new video."
+                 />
+              </View>
+          )
+        }}
+        data={images}
+      />
+    </View>
+  );
+}
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#020410",
+  },
+ 
+  image: {
+    width: "70%",
+    marginTop: "5%",
+    resizeMode: 'contain',
+  },
+
+  title: {
+    fontSize: 35,
+    color: "#3A4065",
+    fontWeight: "bold"
+  }
+});
