c53c2f5e1ebfc46d6fc3904e1c337a50a6cafe57
William <william.mallevays@epitech.eu>
2022-11-05 18:19:03 +0100

    {~ web} Short term fix on private routes

--- web/src/utils/privateRoute.js
+++ web/src/utils/privateRoute.js
@@ -1,31 +1,48 @@
 import React, { useEffect, useState } from "react";
-import { Navigate, Outlet } from "react-router-dom";
-// import { useJwt } from "react-jwt";
+import { useNavigate, Navigate, Outlet } from "react-router-dom";
 import axios from "axios";
+
 
 const PrivateRoute = () => {
   let token = localStorage.getItem("token");
-//   const {decodedToken, isExpired } = useJwt(token);
-  let {isValid, setisValid} = useState(false);
 
-  useEffect(() => {
-    axios
-      .post(
-        `http://localhost:8080/api/auth/isAuthenticated`,
-        JSON.stringify({ token }),
-        { headers: { "Content-Type": "application/json" } }
-      )
-      .then((res) => {
-        console.log("from private Routes", res);
-        console.log(res.data);
-        setisValid(res.data.auth);
-      })
-      .catch((err) => {
-        console.log(err.data);
-      });
-  }, []);
-
-  return isValid ? <Outlet /> : <Navigate to="/login" />;
+  return  token ? <Outlet/> : <Navigate to="/login"/>;
+  
 };
+
+
+// const PrivateRoute = () => {
+//   let token = localStorage.getItem("token");
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const checkAuth = async () => {
+//       try {
+//         const response = await axios.post(
+//                 `http://localhost:8080/api/auth/isAuthenticated`,
+//                 JSON.stringify({ token }),
+//                 { headers: { "Content-Type": "application/json" } }
+//               )
+//         if (response.data.auth === "true" && token) {
+//           setLoading(false);
+
+//         } else {
+//           setLoading(true);
+//         }
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+    
+    
+//     checkAuth();
+//   }, []);
+//   if (loading)
+//       return null;
+
+//   //create a function to check if the api allow the token
+
+//   return  loading ? <Navigate to="/login"/>:  <Outlet/>;
+// };
 
 export default PrivateRoute;
