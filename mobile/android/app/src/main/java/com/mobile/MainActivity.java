d946e28aef8baabdba683f3bd26817ad455e13a2
Antoine <antoine.podvin@epitech.eu>
2022-11-07 14:25:06 +0100

    {+ mobile} Adding splash screen when opening, and now storing login values : user stays logged in when he closes the app.

--- mobile/android/app/src/main/java/com/mobile/MainActivity.java
+++ mobile/android/app/src/main/java/com/mobile/MainActivity.java
@@ -4,13 +4,21 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import static com.blitzapp.animatedsplash.animation.AnimatedText.FIT_CENTER;
+import static com.blitzapp.animatedsplash.animation.Constants.FADE;
+import static com.blitzapp.animatedsplash.animation.Constants.SCALE;
+import static com.blitzapp.animatedsplash.animation.Constants.SPLASH_SLIDE_RIGHT;
+import static com.blitzapp.animatedsplash.animation.Constants.SPLASH_SLIDE_DOWN;
+import static com.blitzapp.animatedsplash.animation.Constants.SPLASH_FADE;
+import static com.blitzapp.animatedsplash.animation.Splash.screenHeight;
+import static com.blitzapp.animatedsplash.animation.Splash.screenWidth;
+import com.blitzapp.animatedsplash.animation.AnimatedObject;
+import com.blitzapp.animatedsplash.animation.GroupAnimation;
+import com.blitzapp.animatedsplash.animation.ObjectAnimation;
+import com.blitzapp.animatedsplash.animation.SingleAnimation;
+import com.blitzapp.animatedsplash.animation.Splash;
 
 public class MainActivity extends ReactActivity {
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -42,6 +50,41 @@ public class MainActivity extends ReactActivity {
       // If you opted-in for the New Architecture, we enable the Fabric Renderer.
       reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
       return reactRootView;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      try {
+        initiateSplash();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+
+    public void initiateSplash() throws Exception {
+      // Create Splash
+      Splash splash = new Splash(getContext());
+      //Set Background Color To View
+      splash.setBackgroundColor("#020410");
+      //Set Hiding Animation for Splash
+      splash.setSplashHideAnimation(SPLASH_FADE);
+      //Set Delay for Splash to hide
+      splash.setSplashHideDelay(1500);
+  
+      //Creating Image and adding its properties and animation
+  
+      AnimatedObject logoimage = new AnimatedObject(R.drawable.area_logo, screenHeight * 0.4, screenWidth * 0.6);
+      splash.addAnimatedImage(logoimage);
+  
+      ObjectAnimation logoimageAnimation1 = new ObjectAnimation(logoimage, FADE, 1000, 0f, 1f, false);
+      ObjectAnimation logoimageAnimation2 = new ObjectAnimation(logoimage, SCALE, 1000, 0f, 1f, 0f, 1f, false);
+      
+      GroupAnimation group1 = new GroupAnimation(1);
+      group1.addAnimation(logoimageAnimation1);
+      group1.addAnimation(logoimageAnimation2);
+    
+      splash.ShowSplash();
     }
 
     @Override
