9220687d2bfe94ce6a0471cccd1f0a9c0ae94422
William <william.mallevays@epitech.eu>
2022-11-07 17:32:21 +0100

    {~ Server } Auth now using passport local strategy and cookies (intead of raw token)

--- server/src/index.js
+++ server/src/index.js
@@ -5,15 +5,9 @@ const port = 4000
 const auth = require('./routes/auth');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Area API Entry Point')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-app.route('/register')
-    .post(auth.register_post)
-
-app.route('/login')
-    .post(auth.login_post)
